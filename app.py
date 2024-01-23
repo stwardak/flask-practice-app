cffe887095822ed40664b35be4acc265ceece6c1
stwardak <148924249+stwardak@users.noreply.github.com>
2024-01-23 11:40:27 -0600

    Add index action

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import db
 
 app = Flask(__name__)
 
@@ -6,3 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route("/photos.json")
+def index():
+    return db.photos_all()
